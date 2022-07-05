e0994050e4f0269060c62f5aedf28d4bdaca157e
LucianoBrumer <lucianobrumer5@gmail.com>
2022-07-05 07:08:12 -0300

    dinamic header

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,24 +2,26 @@ const header = document.querySelector('#header')
 
 header.state = 'visible'
 header.timer = null
+window.scrollY > 0 ? header.mode = 'normal' : header.mode = 'top'
 
 window.addEventListener('scroll', e => {
+    console.log(getComputedStyle(header).backgroundColor, window.scrollY);
 
-    if(window.scrollY != 0) {
-        // header.style.backgroundColor = 'rgba(8, 2, 8, 0.75)'
-        if(getComputedStyle(header).backgroundColor == 'rgba(0, 0, 0, 0)'){
+    if(window.scrollY > 0) {
+        if(header.mode == 'top'){
+            header.mode = 'normal'
             header.animate([
-                {backgroundColor: "rgba(8, 2, 8, 0)"},
-                {backgroundColor: "rgba(8, 2, 8, 0.75)"}
+                {backgroundColor: "rgba(8, 2, 8, 0.75)", padding: "12.5px 20px"}
+                
             ],{
                 duration: 250,
                 fill: 'forwards'
             })
         }
     }else{
+        header.mode = 'top'
         header.animate([
-            {backgroundColor: "rgba(8, 2, 8, 0.75)"},
-            {backgroundColor: "rgba(8, 2, 8, 0)"}
+            {backgroundColor: "rgba(8, 2, 8, 0)", padding: "25px 40px"}
         ],{
             duration: 250,
             fill: 'forwards'
